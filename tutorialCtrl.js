4746d7a976c995b990949cf804c140048da6fba4
zirbem07 <zirbem07@uwosh.edu>
2014-04-25 08:55:49 -0500

    First Commit
    Committing all code

---
+++ tutorialCtrl.js
@@ -0,0 +1,25 @@
+angular.module('tutorialApp', ['ui.router']).config(function($stateProvider, $urlRouterProvider)  {
+
+});
+
+function tutorialCtrl($scope) {
+
+	$scope.superhero = [{name: "Spiderman",
+						type: "hero"}];
+	$scope.alerts = false;
+	$scope.types = '';
+	//make sure this function has the same name as the ng-click attribute associated with your button
+	$scope.addSuperhero = function(){
+		if($scope.name || $scope.type){
+			$scope.superhero.push({
+				name: $scope.name,
+				type: $scope.type
+			});
+			//add something here to clear the name field, maybe just modify the item you added?
+			$scope.name = "";
+			$scope.type = "";
+		} else {
+			$scope.alerts = true;
+		}
+	}
+}
